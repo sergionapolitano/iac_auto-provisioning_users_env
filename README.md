e9c1e7cc0678a2f20afdf3f4f7292a3344f31b3e
Sergio Napolitano <sergio.a.napolitano@gmail.com>
2023-05-01 17:27:35 -0400

    Creating project to submit at dio.me

---
+++ README.md
@@ -0,0 +1,4 @@
+# linux-projeto1-iac
+
+Simple script to create users, groups and shared directories on an new test environment
+This script can be invoked as bootstrap part of new instances.
---
+++ iac1.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# author: Sérgio Napolitano
+# purpose: auto create users, groups and groupdirs for new test environments with shared dirs.
+# Disclaimer: 
+# this script was written just for test purpose and your destination should be only for test environment
+
+# vars section
+USER_GRPS="ADM VEN SEC";
+USERS_ADM="carlos maria joao"
+USERS_VEN="debora sebastiana roberto"
+USERS_SEC="josefina amanda rogerio"
+USER_PASS="Senha123"
+CRYPT_PASS=$(perl -e 'print crypt($ARGV[0], "password")' $USER_PASS)
+
+echo "Creating users and shared dirs..."
+for i in $USER_GRPS ; do
+    echo "groupadd GRP_$i"
+    echo "mkdir /${i,,}"
+    echo "chown root:GRP_$i /${i,,}"
+    echo "chmod 770 /${i,,}"
+done
+echo "mkdir /publico"
+echo "chmod 777 /publico"
+
+echo ""
+echo "#########"
+echo "Creating users based on groups..."
+for i in $USER_GRPS ; do
+    grp=$(echo USERS_${i})	
+    for j in ${!grp} ; do
+       echo "useradd $j -m -s /bin/bash -p $CRYPT_PASS -G GRP_$i"
+    done
+done
+
+
